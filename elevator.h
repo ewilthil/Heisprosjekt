9b4bd23e1111f22126e85319cd33934bd8d53d3a
Marius Thoresen <marius.thoresen@gmail.com>
2012-02-07 21:27:56 +0100

    interface.c oppdatert, kompileringsklar

--- elevator.h
+++ elevator.h
@@ -4,14 +4,24 @@ Spørsmål eller kommentarer?
 #ifndef MAIN_H
 #define MAIN_H
 
+#include "elev.h"
 #include <time.h>
 
 #define NUMBEROFFLOORS 4 
 #define NUMBEROFBUTTONTYPES 3
 #define COMMAND 2
 
-enum direction_t{UP=1, DOWN=-1};
-enum floor_t{FIRST=0,SECOND=1,THIRD=2,FOURTH=3};
+typedef enum {
+	UP=1, 
+	DOWN=-1
+}direction_t;
+
+typedef enum {
+	FIRST=0,
+	SECOND=1,
+	THIRD=2,
+	FOURTH=3
+}floor_t;
 
 
 /*hovedfunksjoner*/
@@ -20,7 +30,7 @@ int ctrl_floorHasOrder();
 void ctrl_handleStop();
 void ctrl_handleEmergencyStop();
 void ctrl_handleDestination();
-void ctrl_addOrderToList(buttonType,floor);
+void ctrl_addOrderToList( elev_button_type_t,  floor_t);
 void ctrl_checkSensor();
 /*Støttefunksjoner*/
 int ctrl_checkOrderInThisDirection();
--- interface.c
+++ interface.c
@@ -1,35 +1,35 @@
-#include "main.h"
+#include "elevator.h"
+#include "tester.h"
 #include "elev.h"
 
 void checkStop(){
 	if(elev_get_stop_signal()){
 		handleEvent(STOP_PRESSED);
 	}	
-	return;
 }
 
 void checkButtons(){
 	elev_button_type_t buttonType;
-	int floor;
+	floor_t floor;
 	
 	buttonType = BUTTON_CALL_UP;
 	for(floor=0; floor<=2; floor++){
 		if(elev_get_button_signal(buttonType, floor)){	
-			addOrderToList(floor,buttonType); 
+			ctrl_addOrderToList(buttonType,floor); 
 		}
 	}
 
 	buttonType = BUTTON_CALL_DOWN;
 	for(floor=1; floor<=3; floor++){
 		if(elev_get_button_signal(buttonType, floor)){	
-			addOrderToList(floor,buttonType); 
+			ctrl_addOrderToList(buttonType,floor); 
 		}
 	}
 
 	buttonType = BUTTON_COMMAND;
 	for(floor=0; floor<=3; floor++){
 		if(elev_get_button_signal(buttonType, floor)){	
-			addOrderToList(floor,buttonType); 
+			ctrl_addOrderToList(buttonType,floor); 
 		}
 	}
 }
