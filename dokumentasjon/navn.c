98d7987860d36c2fac2c306c3ab3713d063721b0
Erik Wilthil <erik.wilthil@gmail.com>
2012-02-07 12:49:10 +0100

    elevator.c

---
+++ dokumentasjon/navn.c
@@ -0,0 +1,19 @@
+/*
+navngivning eks
+heis datastyring
+Erik og Marius
+ADVARSEL: pseudokode enkelte plasser. ikke kompilær
+*/
+int variabelNavn;
+void funksjonsNavn();
+S_STATE //tilstand
+typenavn_t //typer
+ENUM //uten prefix
+ui_function //til klassen UI
+sap_function //til klassen State Action Pair
+fsm_function
+elev_function
+
+int enFunksjon(/*tomt her ved ingen argumenter*/){
+	kodekodekode
+}
---
+++ elevator.c
@@ -0,0 +1,33 @@
+#include "elev.h"
+#include "elevator.h"
+
+static enum floor_t floor;
+static enum direction_t direction;
+static bool destinationMatrix[NUMBEROFBUTTONTYPES][NUMBEROFFLOORS]={
+                      /*1	2	3	4*/
+/*CALL_UP*/{		0,	0,	0,	0},
+/*CALL_DOWN*/{		0,	0,	0,	0},
+/*COMMAND*/{		0,	0,	0,	0}
+};
+
+
+void checkSensor(){
+	if(inFloor()){
+		handleEvent(FLOOR_REACHED);
+	}
+}	
+/*
+floorHasOrder()
+og
+noObstruction()
+er guards for FSM
+*/
+bool floorHasOrder(){
+	return (destinationMatrix[direction][floor]||destionationMatrix[COMMAND][floor]);
+}
+bool noObstruction(){
+	return !(elev_get_obstruction_signal());
+} 
+void addOrderToList(enum elev_button_type_t button,enum floor_t floor){
+	destinationMatrix[button][floor]=1;
+}
